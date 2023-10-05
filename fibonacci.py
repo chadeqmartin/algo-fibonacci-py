a40074f6ca2efcf687d724d64f416af7f8c4a028
chadeqmartin <chadeqmartin@gmail.com>
2023-10-05 16:17:34 -0400

    update with working solution to challenge

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,19 @@
 def fibonacci(n):
-  pass
+  # if n == 0:
+  #   return 0
+  # elif n == 1:
+  #   return 1
+  # else:
+  #   return fibonacci(n - 1) + fibonacci(n - 2)
+
+  fib_list = [0, 1]
+
+  if len(fib_list) <= n:
+    while len(fib_list) <= n:
+      fib_list.append(fib_list[len(fib_list) - 1] + fib_list[len(fib_list) - 2])
+
+  return fib_list[n]
+
+  
 
 
